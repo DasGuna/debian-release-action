47f7adea5e4480943bae28840ac0eca04894a452
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 12:11:00 +1000

    testing amd image

--- docker_setup.sh
+++ docker_setup.sh
@@ -7,11 +7,9 @@ ls -la
 # docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
 # docker run amd64_ros_container:latest
 # # # docker cp test-container:/usr/src/my-workdir/my-outputs .
-# docker rm amd64_ros_container:latest
 
 docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
 docker run arm64_ros_container:latest
-# # docker cp test-container:/usr/src/my-workdir/my-outputs .
-docker rm arm64_ros_container:latest
+docker cp arm64_ros_container:/test.txt .
 
 echo "Container Completed Functions"
--- release.sh
+++ release.sh
@@ -7,5 +7,7 @@ echo "Checking current directory content"
 ls -la
 echo "Current architecture check"
 uname -m
+echo "Creating a test file for output"
+touch test.txt
 # echo "Checking ROS install path"
 # ls -la /opt/ros/
