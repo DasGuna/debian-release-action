b6ed2466e70f4687221efb103caadf07b34ad05f
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 11:48:58 +1000

    testing amd image

--- docker_setup.sh
+++ docker_setup.sh
@@ -3,8 +3,8 @@ set -e
 echo "Setting up and Running Container..."
 ls -la
 docker build -f ./amd64_Dockerfile -t amd64_ros_container .
-docker run -dp 3000:3000 amd64_ros_container
+docker run -dp 3000:3000 amd64_ros_container:latest
 # # docker cp test-container:/usr/src/my-workdir/my-outputs .
-docker rm amd64_ros_container
+docker rm amd64_ros_container:latest
 
 echo "Container Completed Functions"
