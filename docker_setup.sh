c56ce421da084ebcb5d0aa7cd2de0627f13404cd
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 12:53:44 +1000

    tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,6 +6,8 @@ echo "Setting up and Running Container..."
 echo "Path and Contents at this point"
 pwd
 ls -la
+echo "Make a mount point for files"
+mkdir targets
 echo "Running Docker Container for Release..."
 # docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
 # docker run amd64_ros_container:latest
