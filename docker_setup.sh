73403b6b8ea72a1908508e0b4975cf25533de2ed
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 13:21:55 +1000

    matrix arch tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -15,15 +15,14 @@ mkdir targets
 mount_point_path=$(realpath targets/)
 
 echo "Running Docker Container for Release..."
-echo "Checking put characteristics"
 if [[ $INPUT_ARCH == 'amd64' ]]
 then
-    echo "AMD 64 RELEASE"
+    echo "AMD 64 RELEASE Confirmed"
     docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
     docker run -v $mount_point_path:/test_targets amd64_ros_container:latest
-elif [[ $INPUT_ARCH == 'arm64v8' ]]
+elif [[ $INPUT_ARCH == 'arm64' ]]
 then 
-    echo "ARM 64 RELEASE"
+    echo "ARM 64 RELEASE Confirmed"
     docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
     docker run -v $mount_point_path:/test_targets arm64_ros_container:latest
 else
