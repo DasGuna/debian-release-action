e35f3422978afcfe15636dc7e1d71b5c01b83f24
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 11:10:41 +1000

    initial test commit of a composite action - with example code for verification

---
+++ release.sh
@@ -0,0 +1,9 @@
+echo "Running Release Pipeline..."
+echo "Checking current directory path"
+pwd
+echo "Checking current directory content"
+ls -la
+echo "Current architecture check"
+uname -m
+echo "Checking ROS install path"
+ls -la /opt/ros/
