d59ecbe498c8409bb98b34b30217eee0bc9e2adc
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 12:44:56 +1000

    tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -8,13 +8,15 @@ pwd
 ls -la
 echo "Make a mount point for files"
 mkdir targets
+echo "ls path"
+ls $GITHUB_PATH
 echo "Running Docker Container for Release..."
 # docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
 # docker run amd64_ros_container:latest
 # # # docker cp test-container:/usr/src/my-workdir/my-outputs .
 
 docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
-docker run -d -v ./targets:/test_targets arm64_ros_container:latest
+docker run -d -v targets:/test_targets arm64_ros_container:latest
 
 echo "Container Completed Functions"
 echo "Path and Contents at this point"
