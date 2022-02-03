f16cf1a6e386553001e5783f0ae65d12171b6387
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 13:03:46 +1000

    tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,17 +6,23 @@ echo "Setting up and Running Container..."
 echo "Path and Contents at this point"
 pwd
 ls -la
+
+# Reference release entrypoint path
+entrypoint_path=$(realpath release.sh)
+
 echo "Make a mount point for files"
 mkdir targets
 mount_point_path=$(realpath targets/)
+
 
 echo "Running Docker Container for Release..."
 # docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
 # docker run amd64_ros_container:latest
 # # # docker cp test-container:/usr/src/my-workdir/my-outputs .
 
-docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
-docker run -v $mount_point_path:/test_targets arm64_ros_container:latest
+
+# docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
+docker run --name release-container -v $mount_point_path:/test_targets $FROM $entrypoint_path
 
 echo "Container Completed Functions"
 echo "Path and Contents at this point"
