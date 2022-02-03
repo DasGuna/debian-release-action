de941fac666d16dd30d540606fe9cd547ed5b979
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 12:41:28 +1000

    tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -3,13 +3,22 @@
 set -e
 
 echo "Setting up and Running Container..."
+echo "Path and Contents at this point"
+pwd
 ls -la
+echo "Make a mount point for files"
+mkdir targets
+echo "Running Docker Container for Release..."
 # docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
 # docker run amd64_ros_container:latest
 # # # docker cp test-container:/usr/src/my-workdir/my-outputs .
 
 docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
-docker run arm64_ros_container:latest
-docker cp arm64_ros_container:test.txt .
+docker run -d -v ./targets:/test_targets arm64_ros_container:latest
 
 echo "Container Completed Functions"
+echo "Path and Contents at this point"
+pwd
+ls -la
+echo "Files in mount point"
+ls targets
--- release.sh
+++ release.sh
@@ -7,7 +7,9 @@ echo "Checking current directory content"
 ls -la
 echo "Current architecture check"
 uname -m
-echo "Creating a test file for output"
+echo "Creating a test file for output in something folder"
+mkdir test_targets
+cd test_targets
 touch test.txt
 # echo "Checking ROS install path"
 # ls -la /opt/ros/
