8ede0ad95abd22f6d7e5bcd7bbe4fe7b6dc0c382
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-03 13:18:33 +1000

    matrix arch tests

--- docker_setup.sh
+++ docker_setup.sh
@@ -14,15 +14,22 @@ echo "Make a mount point for files"
 mkdir targets
 mount_point_path=$(realpath targets/)
 
-
 echo "Running Docker Container for Release..."
-# docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
-# docker run amd64_ros_container:latest
-# # # docker cp test-container:/usr/src/my-workdir/my-outputs .
-
-
-# docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
-docker run --name release-container -v $mount_point_path:/test_targets $FROM $entrypoint_path
+echo "Checking put characteristics"
+if [[ $INPUT_ARCH == 'amd64' ]]
+then
+    echo "AMD 64 RELEASE"
+    docker build -f ./amd64_Dockerfile -t amd64_ros_container:latest .
+    docker run -v $mount_point_path:/test_targets amd64_ros_container:latest
+elif [[ $INPUT_ARCH == 'arm64v8' ]]
+then 
+    echo "ARM 64 RELEASE"
+    docker build -f ./arm64_Dockerfile -t arm64_ros_container:latest .
+    docker run -v $mount_point_path:/test_targets arm64_ros_container:latest
+else
+    echo "UNKNOWN ARCH"
+    exit -1
+fi
 
 echo "Container Completed Functions"
 echo "Path and Contents at this point"
