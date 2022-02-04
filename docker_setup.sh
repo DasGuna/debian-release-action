d7fd8a5b6d2eaef205e8c3279a368d9320b9baa7
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-04 23:42:14 +1000

    minor cleanup

--- docker_setup.sh
+++ docker_setup.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# echo "Installing some dependencies..."
-# sudo apt install qemu-user-static
-
 echo "Setting up and Running Container..."
 echo "Make a mount point for workspace (if not available)"
 if [ ! -d ws ]; then
